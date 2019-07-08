2797b54ddb9ddd800b26baa4494aa1827e998881
cairnsr <cairnsr@oregonstate.edu>
2019-07-07 18:02:05 -0700

    Delete search.js

--- public/search.js
+++
@@ -1,7 +0,0 @@
-
-function searchCharacterByFirstName() {
-  //get the first name 
-  var first_name_search_string  = document.getElementById('first_name_search_string').value
-  //construct the URL and redirect to it
-  window.location = '/characters/search/' + encodeURI(first_name_search_string)
-}
